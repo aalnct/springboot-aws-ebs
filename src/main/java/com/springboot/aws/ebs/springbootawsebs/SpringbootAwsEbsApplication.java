f938f283c0403293aead4c1578334668caf891d6
Amit Agarwal <amit.agarwal@searshc.com>
2020-10-28 23:08:18 -0500

    Updated buildspec.yml

--- src/main/java/com/springboot/aws/ebs/springbootawsebs/SpringbootAwsEbsApplication.java
+++ src/main/java/com/springboot/aws/ebs/springbootawsebs/SpringbootAwsEbsApplication.java
@@ -2,8 +2,12 @@ package com.springboot.aws.ebs.springbootawsebs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@ComponentScan
+@Configuration
 public class SpringbootAwsEbsApplication {
 
 	public static void main(String[] args) {
