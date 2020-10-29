1c9a23c0dc863be0fe67cb977cecdb46556a62c0
Amit Agarwal <amit.agarwal@searshc.com>
2020-10-28 22:32:47 -0500

    My First commit for springboot aws and Elastic beanstalk

---
+++ src/main/java/com/springboot/aws/ebs/springbootawsebs/SpringbootAwsEbsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.aws.ebs.springbootawsebs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAwsEbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAwsEbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/aws/ebs/springbootawsebs/SpringbootAwsEbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.aws.ebs.springbootawsebs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsEbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
